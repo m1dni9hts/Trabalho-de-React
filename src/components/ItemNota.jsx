900aca16839539d3a9e9dd043e27413450467698
Laura <pizzuttilaura@gmail.com>
2024-07-29 10:15:46 -0300

    commit

--- src/components/ItemNota.jsx
+++ src/components/ItemNota.jsx
@@ -22,33 +22,34 @@ function ItemNota({ Nota, indice, removerNota, atualizarNota }) {
 
   return (
     <div className="nota-item">
-      {Editando ? (
-        <input type="text" value={NovoTexto} onChange={(e) => setNovoTexto(e.target.value)} className="nota-input" />
-      ) : (
-        <div className="nota-text-container">
-          <span className="nota-indice">{indice + 1}.</span>
+      <div className="nota-text-container">
+        <span className="nota-indice">{indice + 1}.</span>
+        {Editando ? (
+          <input type="text" value={NovoTexto} onChange={(e) => 
+          setNovoTexto(e.target.value)} className="nota-input"/>
+        ) : (
           <span className="nota-text">{Nota.texto}</span>
-        </div>
-      )}
+        )}
+      </div>
       <div className="class-Nota">
-        {Editado && <span className="editado">nota editada</span>}
+        {Editado && <span className="editado">item editado</span>}
         <button onClick={Editando ? salvarEdicao : iniciarEdicao} className="edit-btn">
           {Editando ? (
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
-              <path d="M0 0h24v24H0z" fill="none" />
-              <path d="M5 13l4 4L19 7l-1.41-1.41L9 14.17l-2.59-2.58L5 13z" />
+              <path d="M0 0h24v24H0z" fill="none"/>
+              <path d="M5 13l4 4L19 7l-1.41-1.41L9 14.17l-2.59-2.58L5 13z"/>
             </svg>
           ) : (
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-pencil-square" viewBox="0 0 16 16">
-              <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
-              <path fillRule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z" />
+              <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
+              <path fillRule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z"/>
             </svg>
           )}
         </button>
         <button onClick={() => removerNota(Nota.id)} className="delete-btn">
           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash" viewBox="0 0 16 16">
-            <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z" />
-            <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z" />
+            <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5z"/>
+            <path fillRule="evenodd" d="M14 3.5V4h-1v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4H2v-.5A1.5 1.5 0 0 1 3.5 2h9A1.5 1.5 0 0 1 14 3.5zM12.5 3h-9A.5.5 0 0 0 3 3.5V4h10v-.5a.5.5 0 0 0-.5-.5z"/>
           </svg>
         </button>
       </div>
