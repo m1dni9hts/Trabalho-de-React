f886caa41cebe8c9180dbeec46892af161c0ee10
Laura <pizzuttilaura@gmail.com>
2024-07-30 12:05:24 -0300

    commit

--- src/components/ItemNota.jsx
+++ src/components/ItemNota.jsx
@@ -32,7 +32,7 @@ function ItemNota({ Nota, indice, removerNota, atualizarNota }) {
         )}
       </div>
       <div className="class-Nota">
-        {Editado && <span className="editado">item editado</span>}
+        {Editado && <span className="editado">nota  editada</span>}
         <button onClick={Editando ? salvarEdicao : iniciarEdicao} className="edit-btn">
           {Editando ? (
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
